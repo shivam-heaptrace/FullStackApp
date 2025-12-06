582804337c89008f255eb01abaed7076353a5e83
SHIVAM SAHU <shivams@heaptrace.com>
2025-12-06 14:47:34 +0530

    refactor : employeelist

--- frontend/src/components/EmployeeList.jsx
+++ frontend/src/components/EmployeeList.jsx
@@ -4,6 +4,12 @@ import { getEmployees } from "../api/employeeApi";
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
 
+  let thtd = {
+    border: "1px solid black",
+    padding: "8px",
+    textAlign: "left",
+  };
+
   useEffect(() => {
     const fetchEmployees = async () => {
       const res = await getEmployees();
@@ -15,13 +21,26 @@ const EmployeeList = () => {
   return (
     <div>
       <h2>Employee List</h2>
-      <ul>
-        {employees.map((emp) => (
-          <li key={emp.id}>
-            {emp.name} ({emp.email})
-          </li>
-        ))}
-      </ul>
+      <table
+        style={{
+          border: "1px solid black",
+          borderCollapse: "collapse",
+          padding: "20px",
+        }}
+      >
+        <tbody>
+          <tr>
+            <th style={thtd}>Full Name</th>
+            <th style={thtd}>Email ID</th>
+          </tr>
+          {employees.map((emp) => (
+            <tr key={emp.id}>
+              <td style={thtd}>{emp.name}</td>
+              <td style={thtd}>{emp.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
